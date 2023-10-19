ee620e07e24ee666998a19fd72648ac9af2535e3
baonv <baonv@kozo-japan.com>
2023-10-19 09:45:36 +0700

    first commit

---
+++ app/api/members/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function GET(req: Request) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    const server = await db.server.findMany({
+      where: {
+        id: serverId,
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[MEMBER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil"
+    });
+
+    return config;
+  },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'utfs.io',
+            port: '',
+            pathname: '/f/**',
+          },
+        ],
+      },
+    
+}
 
 module.exports = nextConfig
